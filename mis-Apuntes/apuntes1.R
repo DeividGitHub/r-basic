b4ae4c2e1fb964d342df4f5de8b8c3d193bd1461
Juan David Heredia <jdavid35@hotmail.com>
2020-04-20 00:06:14 -0500

    20-04-2020

---
+++ mis-Apuntes/apuntes1.R
@@ -0,0 +1,37 @@
+# Como instalar paquete en R que no necesariamente viene instaladasd e serie
+# install.packages("nombre-paquete", dep = TRUE)
+#library(nombre-del-paquete) carga un paquete ya instalado
+
+
+
+x = (pi^2)/2
+x
+
+y <- cos(pi/4)
+y
+ sin(pi/4)+ cos(pi/4) -> z
+z 
+
+edad <- 30
+nombre = "Juan David"
+edad
+nombre
+pi
+
+2.3
+pi.4 = pi *4
+pi.4
+edad = 34
+
+# Funciones
+
+suma = function(x,y){
+    x+y
+  }
+suma(3,5)
+ 
+
+ls()
+rm(nombre)
+rm(list = ls())
+ls()
